bc5c1c658982f5a5bdf5c4ef0be71d30048ba430
ERFAN.NB <naboureh.1385@gmail.com>
2024-05-16 21:34:34 +0330

    coding [homeId]

--- components/modules/Home.js
+++ components/modules/Home.js
@@ -4,35 +4,35 @@ import React from "react";
 function Home({ title, id, price, meterage, roomCount, img }) {
   return (
     <>
-      <div class="card">
-        <img src={img} alt="House 6" class="card__img" />
-        <h5 class="card__title">{title}</h5>
-        {/* <svg class="card__like">
+      <div className="card">
+        <img src={img} alt="House 6" className="card__img" />
+        <h5 className="card__title">{title}</h5>
+        {/* <svg className="card__like">
           <use xlink:href="img/sprite.svg#icon-heart-full"></use>
         </svg> */}
-        <div class="card__details">
-          <svg class="card__icon">
+        <div className="card__details">
+          {/* <svg className="card__icon">
             <use xlink:href="img/sprite.svg#icon-map-pin"></use>
-          </svg>
-          <p class="card__text">مالدیو</p>
+          </svg> */}
+          <p className="card__text">مالدیو</p>
 
-          {/* <svg class="card__icon">
+          {/* <svg className="card__icon">
             <use xlink:href="img/sprite.svg#icon-profile-male"></use>
           </svg> */}
-          <p class="card__text">{roomCount} اتاق</p>
+          <p className="card__text">{roomCount} اتاق</p>
 
-          {/* <svg class="card__icon">
+          {/* <svg className="card__icon">
             <use xlink:href="img/sprite.svg#icon-expand"></use>
           </svg> */}
-          <p class="card__text">{meterage} متر مربع</p>
+          <p className="card__text">{meterage} متر مربع</p>
 
-          {/* <svg class="card__icon">
+          {/* <svg className="card__icon">
             <use xlink:href="img/sprite.svg#icon-key"></use>
           </svg> */}
-          <p class="card__text">{price.toLocaleString()} میلیون تومان</p>
+          <p className="card__text">{price.toLocaleString()} میلیون تومان</p>
         </div>
 
-        <Link href={`/homes/${id}`} class="btn btn-brown btn-card">
+        <Link href={`/homes/${id}`} className="btn btn-brown btn-card">
           مشاهده ملک
         </Link>
       </div>
--- pages/homes/[id].js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function singleHomeDetailsPge() {
-  return <div>singleHome Details Pge</div>;
-}
-
-export default singleHomeDetailsPge;
