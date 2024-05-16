ba601a502b1c16931043cc41b87b5a3207487e76
ERFAN.NB <naboureh.1385@gmail.com>
2024-05-16 20:55:41 +0330

    create Features.js,Gallery.js,Homes.js,Story.js and coding in files and add files to index.js

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import Features from "@/components/template/index/Features";
+import Gallery from "@/components/template/index/Gallery";
+import Homes from "@/components/template/index/Homes";
+import Story from "@/components/template/index/Story";
+
+import React from "react";
 
 function index() {
   return (
-    <div>Home page</div>
-  )
+    <>
+      <Features />
+      <Story />
+      <Homes />
+      <Gallery />
+    </>
+  );
 }
 
-export default index
+export default index;
